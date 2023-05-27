51e3f154ca0cad965e193f7b4b5b4dea48fa3d4e
Anton Kovalev <camotank12345@gmail.com>
2023-05-27 07:59:58 +0200

    Update 003-ee.sh

--- scripts/003-ee.sh
+++ scripts/003-ee.sh
@@ -2,7 +2,7 @@
 # 003-ee.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
 ## Download the source code.
-REPO_URL="https://github.com/ps2dev/ps2toolchain-ee"
+REPO_URL="https://github.com/vertver/ps2toolchain-ee"
 REPO_FOLDER="ps2toolchain-ee"
 
 # Checking if a specific TAG has been selected, it is passed using parameter $1
